aed4de6998f63fc0b0928ce960a5f3c28eb1d3a1
Nabil <nabil.mirabeau@hotmail.fr>
2024-02-02 08:52:42 +0100

    test prod

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 import "./App.css";
 import * as React from "react";
 import * as ReactDOM from "react-dom/client";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import {
+  createBrowserRouter,
+  createHashRouter,
+  RouterProvider,
+} from "react-router-dom";
 import Accueil from "./components/Accueil";
 import About from "./components/About";
 import Books from "./components/Books";
 import BookPage from "./components/BookPage";
 import BookOne from "./components/BookOne";
 
-const router = createBrowserRouter([
+const router = createHashRouter([
   {
     path: "/",
     element: <Accueil />,
